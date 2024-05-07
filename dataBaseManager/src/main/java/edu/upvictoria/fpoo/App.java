9c9e692bbd3af8e7addb5fc8abdec493ba04ffc6
Deisy Dalila De La Fuente Alvarado <2230162@upv.edu.mx>
2024-05-06 22:20:44 -0600

    1 commit

--- dataBaseManager/src/main/java/edu/upvictoria/fpoo/App.java
+++ dataBaseManager/src/main/java/edu/upvictoria/fpoo/App.java
@@ -1,13 +1,10 @@
 package edu.upvictoria.fpoo;
+import java.io.*;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void App(String[] args) {
+        SimpleBaseDeDatos db = new SimpleBaseDeDatos("ruta/por/defecto");
+        db.usar("nueva/ruta/de/trabajo");
+        db.mostrarTablas();
     }
 }
