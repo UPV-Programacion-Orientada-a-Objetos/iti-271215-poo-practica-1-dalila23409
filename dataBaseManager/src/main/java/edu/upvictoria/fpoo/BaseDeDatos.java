d7c186254916f8c96d2c99796c68baa65fc8b6db
Deisy Dalila De La Fuente Alvarado <2230162@upv.edu.mx>
2024-05-09 11:30:21 -0600

    4 commit

--- dataBaseManager/src/main/java/edu/upvictoria/fpoo/BaseDeDatos.java
+++ dataBaseManager/src/main/java/edu/upvictoria/fpoo/BaseDeDatos.java
@@ -3,23 +3,18 @@ import java.io.*;
 
 public class BaseDeDatos {
     private String directorioDeTrabajo;
-
     public BaseDeDatos(String directorioDeTrabajo) {
         this.directorioDeTrabajo = directorioDeTrabajo;
     }
-
     public void usar(String ruta) {
         this.directorioDeTrabajo = ruta;
     }
-
     public void mostrarTablas() {
         Tabla.mostrarTablas(directorioDeTrabajo);
     }
-
     public void crearTabla(String nombreTabla, String[] columnas) {
         Tabla.crearTabla(directorioDeTrabajo, nombreTabla, columnas);
     }
-
     public void eliminarTabla(String nombreTabla) {
         Tabla.eliminarTabla(directorioDeTrabajo, nombreTabla);
     }
@@ -27,15 +22,12 @@ public class BaseDeDatos {
     public void insertar(String nombreTabla, String[] valores) {
         Tabla.insertar(directorioDeTrabajo, nombreTabla, valores);
     }
-
     public void eliminar(String nombreTabla, String condicion) {
         Tabla.eliminar(directorioDeTrabajo, nombreTabla, condicion);
     }
-
     public void actualizar(String nombreTabla, String[] columnas, String condicion) {
         Tabla.actualizar(directorioDeTrabajo, nombreTabla, columnas, condicion);
     }
-
     public void seleccionar(String nombreTabla, String columnas, String condicion) {
         Tabla.seleccionar(directorioDeTrabajo, nombreTabla, columnas, condicion);
     }
---
+++ dataBaseManager/src/main/java/edu/upvictoria/fpoo/Excepciones.java
@@ -0,0 +1,4 @@
+package edu.upvictoria.fpoo;
+
+public class Excepciones {
+}
