bbcbbaaef9cafddfcae93d3c8f4fb04e89bca7b4
Deisy Dalila De La Fuente Alvarado <2230162@upv.edu.mx>
2024-05-09 23:10:29 -0600

    7 commit

--- dataBaseManager/src/main/java/edu/upvictoria/fpoo/BaseDeDatos.java
+++ dataBaseManager/src/main/java/edu/upvictoria/fpoo/BaseDeDatos.java
@@ -2,34 +2,37 @@ package edu.upvictoria.fpoo;
 import java.io.*;
 
 public class BaseDeDatos {
-    private String directorioDeTrabajo;
-    public BaseDeDatos(String directorioDeTrabajo) {
-        this.directorioDeTrabajo = directorioDeTrabajo;
-    }
-    public void usar(String ruta) {
-        this.directorioDeTrabajo = ruta;
-    }
-    public void mostrarTablas() {
-        Tabla.mostrarTablas(directorioDeTrabajo);
-    }
-    public void crearTabla(String nombreTabla, String[] columnas) {
-        Tabla.crearTabla(directorioDeTrabajo, nombreTabla, columnas);
-    }
-    public void eliminarTabla(String nombreTabla) {
-        Tabla.eliminarTabla(directorioDeTrabajo, nombreTabla);
-    }
-    public void insertar(String nombreTabla, String[] valores) {
-        Tabla.insertar(directorioDeTrabajo, nombreTabla, valores);
-    }
-    public void eliminar(String nombreTabla, String condicion) {
-        Tabla.eliminar(directorioDeTrabajo, nombreTabla, condicion);
-    }
-    public void actualizar(String nombreTabla, String[] columnas, String condicion) {
-        Tabla.actualizar(directorioDeTrabajo, nombreTabla, columnas, condicion);
-    }
-    public void seleccionar(String nombreTabla, String columnas, String condicion) {
-        Tabla.seleccionar(directorioDeTrabajo, nombreTabla, columnas, condicion);
-    }
-    public void (String nombreTabla, String columnas, String condicion) {
+    private String dirTrabajo;
 
+    public BaseDeDatos(String dirTrabajo) {
+        this.dirTrabajo = dirTrabajo;
     }
+
+    public void usar(String ruta) {
+        this.dirTrabajo = ruta;
+    }
+
+    public void mostrarTablas() {
+        Tabla.mostrar(dirTrabajo);
+    }
+
+    public void crearTabla(String nombreTabla, String[] columnas) {
+        Tabla.crear(dirTrabajo, nombreTabla, columnas);
+    }
+
+    public void eliminarTabla(String nombreTabla) {
+        Tabla.eliminar(dirTrabajo, nombreTabla);
+    }
+
+    public void insertar(String nombreTabla, String[] valores) {
+        Tabla.insertar(dirTrabajo, nombreTabla, valores);
+    }
+
+    public void eliminar(String nombreTabla, String condicion) {
+        Tabla.eliminar(dirTrabajo, nombreTabla, condicion);
+    }
+
+    public void actualizar(String nombreTabla, String[] columnas, String condicion) {
+        Tabla.actualizar(dirTrabajo, nombreTabla, columnas, condicion);
+    }
+}
