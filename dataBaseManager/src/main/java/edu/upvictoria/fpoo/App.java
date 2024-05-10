c76a4c908fb97def28c03095359e86cd4b0c656e
Deisy Dalila De La Fuente Alvarado <2230162@upv.edu.mx>
2024-05-09 23:12:18 -0600

    8 commit

--- dataBaseManager/src/main/java/edu/upvictoria/fpoo/App.java
+++ dataBaseManager/src/main/java/edu/upvictoria/fpoo/App.java
@@ -2,9 +2,20 @@ package edu.upvictoria.fpoo;
 import java.io.*;
 
 public class App {
-    public static void App(String[] args) {
-        BaseDeDatos db = new BaseDeDatos("");
-        db.usar("");
-        db.mostrarTablas();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BaseDeDatos baseDeDatos = new BaseDeDatos();
+        InterpreteComandos interpreteComandos = new InterpreteComandos(baseDeDatos);
+
+        while (true) {
+            System.out.print("SQL> ");
+            String comando = scanner.nextLine();
+            if (comando.equalsIgnoreCase("salir")) {
+                System.out.println("Saliendo...");
+                break;
+            }
+            interpreteComandos.interpretarComando(comando);
+        }
+        scanner.close();
     }
-}
+
